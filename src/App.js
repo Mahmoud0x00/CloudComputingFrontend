62172e1c56f80d596962d2773644fb0e32baead3
ahmedwaleedxx <96238616+ahmedwaleedxx@users.noreply.github.com>
2022-12-11 21:31:01 +0200

    Waleed (#2)
    * get articles w gearticlebyid
    
    et3amalo w sha8aleen yaseyyaaa333
    
    * last
    
    * heheheeeeh yaseyyaa333
    
    * aywa baa

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter,Route, Routes } from "react-router-dom";
 import AuthProvider from "./store/AuthProvider";
 import Layout from "./UI/Layout/layout";
 import Home from "./pages/home";
-
+import HelpPage from "./pages/helpPage";
+import CreateArticle from "./pages/createArticle";
 import Login from "./pages/Login";
 import SignUpPage from "./pages/SignUpPage";
+import GetArticleByID from "./pages/getArticleByID";
+import AddArticleComment from "./pages/AddArticleComment";
+import GetArticleComments from "./pages/getArticleComments";
 const App = () => {
   return (
     <AuthProvider>
@@ -16,6 +20,12 @@ const App = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/help" element={<HelpPage />} />
+            <Route path="/help/article/:id" element={<GetArticleByID />} />
+            <Route path="/createArticleComment/:id" element={<AddArticleComment />} />
+            <Route path="/help/createArticle" element={<CreateArticle />} />
+            <Route path="/help/article/comments/:id" element={<GetArticleComments />} />
+
           </Routes>
         </Layout>
       </BrowserRouter>
